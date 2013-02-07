66e6e07a4b57bc8f096c4d228ceb1b3abcc3152e
Derek Watson <watson@dcw.ca>
2013-02-07 00:11:56 -0500

    Refactored SendMessageResponse

--- PostageApp/SendMessageResponse.cs
+++ PostageApp/SendMessageResponse.cs
@@ -24,6 +24,34 @@ namespace PostageApp
 
         public SendMessageResponse(string json)
         {
+            ParseJson(json);            
+        }
+
+        private SendMessageResponseStatus ParseStatus(string status)
+        {
+            switch (status)
+            {
+                case "ok":
+                    return SendMessageResponseStatus.Ok;
+                case "bad_request":
+                    return SendMessageResponseStatus.BadRequest;
+                case "not_found":
+                    return SendMessageResponseStatus.NotFound;
+                case "invalid_json":
+                    return SendMessageResponseStatus.InvalidJson;
+                case "unauthorized":
+                    return SendMessageResponseStatus.Unauthorized;
+                case "call_error":
+                    return SendMessageResponseStatus.CallError;
+                case "precondition_failed":
+                    return SendMessageResponseStatus.PreconditionFailed;
+                default:
+                    return SendMessageResponseStatus.Unknown;
+            }            
+        }
+
+        private void ParseJson(string json)
+        {
             var o = JObject.Parse(json);
 
             if (o["response"] != null)
@@ -35,34 +63,8 @@ namespace PostageApp
                     ErrorMessage = o["response"]["message"].ToString();
 
                 if (o["response"]["status"] != null)
-                    switch (o["response"]["status"].ToString())
-                    {
-                        case "ok":
-                            Status = SendMessageResponseStatus.Ok;
-                            break;
-                        case "bad_request":
-                            Status = SendMessageResponseStatus.BadRequest;
-                            break;
-                        case "not_found":
-                            Status = SendMessageResponseStatus.NotFound;
-                            break;
-                        case "invalid_json":
-                            Status = SendMessageResponseStatus.InvalidJson;
-                            break;
-                        case "unauthorized":
-                            Status = SendMessageResponseStatus.Unauthorized;
-                            break;
-                        case "call_error":
-                            Status = SendMessageResponseStatus.CallError;
-                            break;
-                        case "precondition_failed":
-                            Status = SendMessageResponseStatus.PreconditionFailed;
-                            break;
-                        default:
-                            Status = SendMessageResponseStatus.Unknown;
-                            break;
-                    }
-                }
+                    Status = ParseStatus(o["response"]["status"].ToString());
+            }
 
             if (o["data"] != null)
             {
