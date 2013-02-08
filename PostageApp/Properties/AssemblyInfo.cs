7110a10fe78f4d76f8c5b1ca1229d40d54ba09d1
Derek Watson <watson@dcw.ca>
2013-02-08 13:40:33 -0500

    Added Package.nuspec, README.md

--- PostageApp/Properties/AssemblyInfo.cs
+++ PostageApp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PostageApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PostageApp Client Library for .NET 3.5+")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PostageApp")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("PostageApp")]
+[assembly: AssemblyProduct("PostageApp API")]
+[assembly: AssemblyCopyright("Copyright © PostageApp 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
