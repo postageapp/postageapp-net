047ab856568cfa0701a7dbd78ae73483d3f0e7d7
Vadim Orekhov <vorechov@svyaznoy.ru>
2018-07-02 10:32:15 +0100

    improvedDI

--- src/PostageApp.Extensions.DependencyInjection/DependencyInjection/HttpPostageAppServiceCollectionExtensions.cs
+++ src/PostageApp.Extensions.DependencyInjection/DependencyInjection/HttpPostageAppServiceCollectionExtensions.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 
-using Microsoft.Extensions.DependencyInjection;
-
 using PostageApp.Abstractions;
 using PostageApp.Http;
 
-namespace PostageApp.Extensions.DependencyInjection.DependencyInjection
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class HttpPostageAppServiceCollectionExtensions
     {
-        public static IHttpClientBuilder AddPostageAppClient(this IServiceCollection services)
+        public static IHttpClientBuilder AddPostageAppClient(this IServiceCollection services, Action<HttpPostageAppClientOptions> configureAction = null)
         {
             if (services == null)
             {
                 throw new ArgumentNullException(nameof(services));
+            }
+
+            if (configureAction != null)
+            {
+                services.Configure(configureAction);
             }
 
             return services.AddHttpClient<IPostageAppClient, HttpPostageAppClient>();
